b8e00c9a561b5351a894de60b8e2fd14041a463d
Jared Ricks <jvaun20@gmail.com>
2024-06-02 15:06:00 -0400

    Program now prompts the user if they would like to flip the coin. The coin can be flipped any number of times before they quit the application.

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,32 @@
 import java.util.Random;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Random random = new Random();
+        Scanner scanner = new Scanner(System.in);
 
-        int flipResult = random.nextInt(2);
+        String userResponse;
+        while (true) {
+            System.out.print("Would you like to flip a coin? (yes/no): ");
+            userResponse = scanner.nextLine().trim().toLowerCase();
 
-        if (flipResult == 0) {
-            System.out.println("Heads");
-        } else {
-            System.out.println("Tails");
+            if (userResponse.equals("yes") || userResponse.equals("y")) {
+                int flipResult = random.nextInt(2);
+
+                if (flipResult == 0) {
+                    System.out.println("Heads");
+                } else {
+                    System.out.println("Tails");
+                }
+            } else if (userResponse.equals("no") || userResponse.equals("n")) {
+                System.out.println("Goodbye!");
+                break;
+            } else {
+                System.out.println("Invalid response. Please enter 'yes' or 'no'.");
+            }
         }
+
+        scanner.close();
     }
 }
